17b387e9a7595cdc89b837b7b69badbdecb6574f
Scott Lewis <scottcl@outlook.com>
2024-01-20 13:38:01 -0500

    Setting up android and camera

--- src/app/modal-view-tooth/modal-view-tooth.page.ts
+++ src/app/modal-view-tooth/modal-view-tooth.page.ts
@@ -78,23 +78,25 @@ export class ModalViewToothPage implements OnInit {
     this.imageFailed = true;
   }
 
-  takePicture(){
-    const takePicture = async () => {
+  async takePicture() {
+    try {
       const image = await Camera.getPhoto({
         quality: 90,
         allowEditing: true,
         resultType: CameraResultType.Uri
       });
-    
+
       // image.webPath will contain a path that can be set as an image src.
       // You can access the original file using image.path, which can be
       // passed to the Filesystem API to read the raw data of the image,
       // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
       var imageUrl = image.webPath;
-    
+
       // Can be set to the src of an image now
       this.imageUrl = imageUrl ?? "";
-    };
+    } catch (err) {
+      console.error("takePicture error: ", err);
+    }
   }
 
   descriptionChange(e: any) {
