be219549959d67b4beefe577498a47c78ca97130
Scott Lewis <scottcl@outlook.com>
2024-02-21 18:28:38 -0500

    Cleanup and sonarqube issues

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -5,17 +5,17 @@ import { GeocodeService } from '../geocode.service';
 import { CollectionService } from '../collection.service';
 
 export function appInitializer(storageService: StorageService, firebaseAuthService: FirebaseAuthService, collectionService: CollectionService, loggerService: LoggerService, geocodeService: GeocodeService) {
-    return () => new Promise(async (resolve: any) => {
+    return () => new Promise((resolve: any) => {
         console.debug("appInitializer starting");
-        storageService.init().then(async res => {
+        storageService.init().then(res => {
             console.debug("StorageService init finished inside appInitializer");
-            firebaseAuthService.initialize().then(async res => {
+            firebaseAuthService.initialize().then(res => {
                 console.debug("firebaseAuthService init finished inside appInitializer");
-                collectionService.init().then(async res => {
+                collectionService.init().then(res => {
                     console.debug("gameService init finished inside appInitializer");
-                    loggerService.init().then(async res => {
+                    loggerService.init().then(res => {
                         console.debug("loggerService init finished inside appInitializer");
-                        geocodeService.init().then(async res => {
+                        geocodeService.init().then(res => {
                             console.debug("geocodeService init finished inside appInitializer");
                             resolve();
                         });
