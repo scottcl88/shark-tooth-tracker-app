991d1a1bf83c0462da1fa7273ad8bbb16d389480
Scott Lewis <scottcl88code@gmail.com>
2025-09-14 10:55:18 -0400

    Upgrading angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,45 +16,37 @@ import { LoggerService } from './logger.service';
 import { appInitializer } from './_helpers';
 import { DateFnsModule } from 'ngx-date-fns';
 import { GeocodeService } from './geocode.service';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CollectionService } from './collection.service';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFunctions, provideFunctions } from '@angular/fire/functions';
 import { NgxGpAutocompleteModule } from '@angular-magic/ngx-gp-autocomplete';
-import { Loader } from '@googlemaps/js-api-loader';
+import { ModalViewToothPageModule } from './modal-view-tooth/modal-view-tooth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
+  bootstrap: [AppComponent], imports: [
     BrowserModule,
     IonicModule.forRoot({ innerHTMLTemplatesEnabled: true }),
     DateFnsModule.forRoot(),
     AppRoutingModule,
-    HttpClientModule,
     NgxGpAutocompleteModule,
     IonicStorageModule.forRoot({
       driverOrder: [cordovaSQLiteDriver._driver, Drivers.IndexedDB, Drivers.LocalStorage]
     }),
     CommonModule,
+    RouterModule,
     ModalSignInPageModule,
     ModalSignInEncouragementPageModule,
-    RouterModule,    
-    provideFirebaseApp(() => initializeApp(
-      { "projectId": "shark-tooth-tracker", "appId": "1:403064580432:web:40288c628e26a1e8a8a455", "databaseURL": "https://shark-tooth-tracker-default-rtdb.firebaseio.com", "storageBucket": "shark-tooth-tracker.appspot.com", "apiKey": "AIzaSyBRDTBIiPMYYBvp6bIOsPXf-Id9uXkLh4M", "authDomain": "shark-tooth-tracker.firebaseapp.com", "messagingSenderId": "403064580432", "measurementId": "G-C5TN65XBDG" }
-    )),
-    provideFunctions(() => getFunctions()),    
-  ],
-  providers: [
-    {
-      provide: Loader,
-      useValue: new Loader({
-        apiKey: 'AIzaSyBRDTBIiPMYYBvp6bIOsPXf-Id9uXkLh4M',
-        libraries: ['places']
-      })
-    },
+    ModalViewToothPageModule,
+    NgxGpAutocompleteModule.forRoot({
+        loaderOptions: {
+            apiKey: 'AIzaSyBRDTBIiPMYYBvp6bIOsPXf-Id9uXkLh4M',
+            libraries: ['places']
+        }
+    })
+  ], providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [StorageService, FirebaseAuthService, CollectionService, LoggerService, GeocodeService] },
-  ],
-  bootstrap: [AppComponent],
+    provideHttpClient(withInterceptorsFromDi()),
+  ]
 })
 export class AppModule { }
